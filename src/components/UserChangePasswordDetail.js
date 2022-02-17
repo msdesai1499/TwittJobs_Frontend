6edcd16809fe049fb11e1bb0f5cbfc8395f6d623
ssonawane511 <ssonawane511@gmail.com>
2022-02-17 09:25:41 +0530

    fixes

--- src/components/UserChangePasswordDetail.js
+++ src/components/UserChangePasswordDetail.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
